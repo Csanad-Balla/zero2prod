9172d63ff0158ab7cfc520a3ee83c5dbcaffc64f
Csanád Balla <balla.e.csanad@gmail.com>
2025-02-15 20:57:50 +0100

    Reorganized project, made space for configuration

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,7 @@
 use std::net::TcpListener;
 
-use zero2prod::run;
-
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
-    run(listener)?.await
+    zero2prod::startup::run(listener)?.await
 }
