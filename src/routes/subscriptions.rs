2b5428774e0ce68f0c3acf31b210bcffbcc5cbf9
Csanád Balla <balla.e.csanad@gmail.com>
2025-04-03 22:10:28 +0200

    Update Dependencies

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -3,8 +3,8 @@ use actix_web::HttpResponse;
 
 #[derive(serde::Deserialize)]
 pub struct FormData {
-    email: String,
-    name: String,
+    _email: String,
+    _name: String,
 }
 
 pub async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
